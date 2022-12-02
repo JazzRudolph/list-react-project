5eae9469d40d79a7a60e8b496c6d3c000a5f6217
Jazz Rudolph <jazzrudolph92@outlook.com>
2022-12-01 19:48:38 -0500

    Updated code for time and it now auto refresh every 200 milliseconds.

--- src/component/Date/Date.js
+++ src/component/Date/Date.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 export default function App() {
-  const today = new Date();
-  const date = `${today.getMonth()+1}/${today.getDate()}/${today.getFullYear()}`;
+  const date = new Date().toLocaleDateString();
 
   return (
     <div className="">
--- src/component/Time/Time.js
+++ src/component/Time/Time.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default function Time() {
-    const date = new Date();
-    const time = date.getHours() + ':' + date.getMinutes() + ":" + date.getSeconds();
+    const [time, setTime] = React.useState(
+        new Date().toLocaleTimeString()
+    );
 
-    
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            const newTime = new Date().toLocaleTimeString();
+            setTime(newTime);
+        }, 200);
+        return () => clearInterval(interval);
+      }, []);    
 
 
 
